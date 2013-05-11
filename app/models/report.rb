aeb066bdcd6db8517a873c5fc0ef44aec50c7335
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2013-05-11 23:40:24 +0100

    Use constant lists of options for status/weather.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,9 @@
 class Report < ActiveRecord::Base
+  Statuses = ["Open", "Closed"]
+  Weathers = ["Good", "Fair", "Poor"]
+
   belongs_to :source
   validates :source_id, presence: true
-  validates :weather, presence: true, inclusion: { in: ["Good", "Fair", "Poor"] }
-  validates :status, presence: true, inclusion: { in: ["Open", "Closed"] }
+  validates :weather, presence: true, inclusion: { in: Weathers }
+  validates :status, presence: true, inclusion: { in: Statuses }
 end
